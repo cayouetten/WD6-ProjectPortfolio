1a897c96743c348bb9c13ff096be54e0a4ad5d71
Nicole Cayouette <nicole.cay@yahoo.com>
2017-09-07 00:07:25 -0400

    Shopping Cart Follows 1&2
    Laravel with Composer, MAMP, MySql

---
+++ Solo/WD6i/resources/views/shop/index.blade.php
@@ -0,0 +1,97 @@
+@extends('layouts/master')
+
+<!--TITLE-->
+@section('title')
+  Shop at WD6I - Laravel
+@endsection
+
+<!--CONTENT-->
+@section('content')
+  <div class="row">
+    <div class="col-sm-6 col-md-4">
+      <div class="thumbnail">
+        <img src="https://i0.wp.com/images.phpgang.com/2016/03/Learning-PHP-A-Pain-Free-Introduction-to-Building-Interactive-Websites.jpg?resize=381%2C499" alt="..." class="image-responsive">
+        <div class="caption">
+          <h3>Product Title</h3>
+          <p class="description">Bacon ipsum dolor amet tri-tip pork shankle, adipisicing nisi ipsum est ut eiusmod kielbasa tongue officia exercitation drumstick aute. Drumstick id non in voluptate est veniam fatback shoulder ea flank.</p>
+          <div class="clearfix">
+            <div class="pull-left price">$12.99</div>
+            <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <div class="col-sm-6 col-md-4">
+      <div class="thumbnail">
+        <img src="https://i0.wp.com/images.phpgang.com/2016/03/Learning-PHP-A-Pain-Free-Introduction-to-Building-Interactive-Websites.jpg?resize=381%2C499" alt="..." class="image-responsive">
+        <div class="caption">
+          <h3>Product Title</h3>
+          <p class="description">Bacon ipsum dolor amet tri-tip pork shankle, adipisicing nisi ipsum est ut eiusmod kielbasa tongue officia exercitation drumstick aute. Drumstick id non in voluptate est veniam fatback shoulder ea flank.</p>
+          <div class="clearfix">
+            <div class="pull-left price">$12.99</div>
+            <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <div class="col-sm-6 col-md-4">
+      <div class="thumbnail">
+        <img src="https://i0.wp.com/images.phpgang.com/2016/03/Learning-PHP-A-Pain-Free-Introduction-to-Building-Interactive-Websites.jpg?resize=381%2C499" alt="..." class="image-responsive">
+        <div class="caption">
+          <h3>Product Title</h3>
+          <p class="description">Bacon ipsum dolor amet tri-tip pork shankle, adipisicing nisi ipsum est ut eiusmod kielbasa tongue officia exercitation drumstick aute. Drumstick id non in voluptate est veniam fatback shoulder ea flank.</p>
+          <div class="clearfix">
+            <div class="pull-left price">$12.99</div>
+            <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-sm-6 col-md-4">
+      <div class="thumbnail">
+        <img src="https://i0.wp.com/images.phpgang.com/2016/03/Learning-PHP-A-Pain-Free-Introduction-to-Building-Interactive-Websites.jpg?resize=381%2C499" alt="..." class="image-responsive">
+        <div class="caption">
+          <h3>Product Title</h3>
+          <p class="description">Bacon ipsum dolor amet tri-tip pork shankle, adipisicing nisi ipsum est ut eiusmod kielbasa tongue officia exercitation drumstick aute. Drumstick id non in voluptate est veniam fatback shoulder ea flank.</p>
+          <div class="clearfix">
+            <div class="pull-left price">$12.99</div>
+            <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <div class="col-sm-6 col-md-4">
+      <div class="thumbnail">
+        <img src="https://i0.wp.com/images.phpgang.com/2016/03/Learning-PHP-A-Pain-Free-Introduction-to-Building-Interactive-Websites.jpg?resize=381%2C499" alt="..." class="image-responsive">
+        <div class="caption">
+          <h3>Product Title</h3>
+          <p class="description">Bacon ipsum dolor amet tri-tip pork shankle, adipisicing nisi ipsum est ut eiusmod kielbasa tongue officia exercitation drumstick aute. Drumstick id non in voluptate est veniam fatback shoulder ea flank.</p>
+          <div class="clearfix">
+            <div class="pull-left price">$12.99</div>
+            <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <div class="col-sm-6 col-md-4">
+      <div class="thumbnail">
+        <img src="https://i0.wp.com/images.phpgang.com/2016/03/Learning-PHP-A-Pain-Free-Introduction-to-Building-Interactive-Websites.jpg?resize=381%2C499" alt="..." class="image-responsive">
+        <div class="caption">
+          <h3>Product Title</h3>
+          <p class="description">Bacon ipsum dolor amet tri-tip pork shankle, adipisicing nisi ipsum est ut eiusmod kielbasa tongue officia exercitation drumstick aute. Drumstick id non in voluptate est veniam fatback shoulder ea flank.</p>
+          <div class="clearfix">
+            <div class="pull-left price">$12.99</div>
+            <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
