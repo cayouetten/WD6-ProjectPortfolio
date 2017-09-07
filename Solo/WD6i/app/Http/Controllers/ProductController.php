010f82e6beced08d5e59213ab971c357c90dcdfc
Nicole Cayouette <nicole.cay@yahoo.com>
2017-09-07 12:14:52 -0400

    Follows 8-9
    Cart views, item addition

--- Solo/WD6i/app/Http/Controllers/ProductController.php
+++ Solo/WD6i/app/Http/Controllers/ProductController.php
@@ -6,6 +6,8 @@ use App\Product;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Session;
+use App\Cart;
 
 class ProductController extends Controller
 {
@@ -16,6 +18,22 @@ class ProductController extends Controller
 
     public function getAddToCart(Request $request, $id){
     	$product = Product::find($id);
-    	
+
+    	$oldCart = Session::has('cart') ? Session::get('cart') : null;
+
+    	$cart = new Cart($oldCart);
+    	$cart->add($product, $product->id);
+
+    	$request->session()->put('cart', $cart);
+    	return redirect()->route('product/index');
+    }
+
+    public function getCart(){
+    	if(!Session::has('cart')){
+    		return view('shop/shopping-cart');
+    	}
+    	$oldCart = Session::get('cart');
+    	$cart = new Cart($oldCart);
+    	return view('shop/shopping-cart', ['products' => $cart->items, 'totalPrice' => $cart->totalPrice]);
     }
 }
